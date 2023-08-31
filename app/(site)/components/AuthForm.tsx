4a76d97dba3e54bff6b27a42be9e74920096d750
qi yuhang <qiyuhang@thinkerx.com>
2023-08-31 22:24:48 +0800

    feat: register auth route setup

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import axios from "axios";
 import { Button, Input } from "@/app/components";
 import AuthSocialButton from "./AuthSocialButton";
 import { useState, useCallback } from "react";
@@ -31,6 +31,7 @@ const AuthForm = () => {
     setIsLoading(true);
     if (variant === "REGISTER") {
       // Axios Register
+      axios.post("/api/register", data);
     }
     if (variant === "LOGIN") {
       // NextAuth Sigin
